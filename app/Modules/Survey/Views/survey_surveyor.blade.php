107231d7c4078fce64ce97675bb542491175edca
masulfauzi <34361671+masulfauzi@users.noreply.github.com>
2023-06-27 12:52:36 +0700

    tambah info koordinat

--- app/Modules/Survey/Views/survey_surveyor.blade.php
+++ app/Modules/Survey/Views/survey_surveyor.blade.php
@@ -58,6 +58,40 @@
     <script>
         $(document).ready(function(){
 
+            L.CursorHandler = L.Handler.extend({
+
+                addHooks: function () {
+                    this._popup = new L.Popup();
+                    this._map.on('mouseover', this._open, this);
+                    this._map.on('mousemove', this._update, this);
+                    this._map.on('mouseout', this._close, this);
+                },
+
+                removeHooks: function () {
+                    this._map.off('mouseover', this._open, this);
+                    this._map.off('mousemove', this._update, this);
+                    this._map.off('mouseout', this._close, this);
+                },
+
+                _open: function (e) {
+                    this._update(e);
+                    this._popup.openOn(this._map);
+                },
+
+                _close: function () {
+                    this._map.closePopup(this._popup);
+                },
+
+                _update: function (e) {
+                    this._popup.setLatLng(e.latlng)
+                        .setContent(e.latlng.toString());
+                }
+
+
+            });
+
+            L.Map.addInitHook('addHandler', 'cursor', L.CursorHandler);
+
             L.DomEvent.on(document.getElementById('refreshButton'), 'click', function(){
                 // map.locate({setView: true, maxZoom: 18});
 
@@ -107,7 +141,8 @@
 
             var map = L.map('map', {
                 doubleClickZoom: false,
-                layers: [googleSat]
+                layers: [googleSat],
+                cursor: true
             }).locate({setView: true, maxZoom: 18});
 
             var baseMaps = {
